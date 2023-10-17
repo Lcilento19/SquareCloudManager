424966c420ff2b28076b36af017cc8fb59575c69
Lcilento19 <krony9war@gmail.com>
2023-10-17 15:17:27 -0300

    Update vite.config.js

--- SquareCloudManager/vite.config.js
+++ SquareCloudManager/vite.config.js
@@ -3,5 +3,23 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5000 },
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://square-cloud-manager.vercel.app",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+    headers: {
+      source: "/(.*)",
+      headers: [
+        { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
+        {
+          key: "Cross-Origin-Opener-Policy",
+          value: " same-origin-allow-popups",
+        },
+      ],
+    },
+  },
 });
